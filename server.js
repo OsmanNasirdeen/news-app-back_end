ad2f77320abb11997b09eeb4d72dbd248ce2150c
osman nasirdeen <nasirdeenosman@gmail.com>
2022-12-01 08:20:15 +0000

    initialised server

---
+++ server.js
@@ -0,0 +1,25 @@
+import fetch from "node-fetch";
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+const port = 700;
+
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  fetch(process.env.api_url)
+    .then((response) => {
+      response.json().then((data) => {
+        const { articles } = data;
+        res.json({ articles: articles });
+      });
+    })
+    .catch((error) => {
+      res.json({ articles: "server down! try again in few minutes" });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`server is listening at port:${process.env.port}  `);
+});
