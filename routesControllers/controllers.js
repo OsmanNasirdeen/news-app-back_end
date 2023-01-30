1cb8b0c78afaf996bf8b464c6eaab7ff2eb8b1b6
osman nasirdeen <nasirdeenosman@gmail.com>
2023-01-30 14:15:06 +0000

    new file:   controllers.js module added containing routes functions server.js modified : routes names updated and routes functions moved to controllers

---
+++ routesControllers/controllers.js
@@ -0,0 +1,52 @@
+import fetch from "node-fetch";
+
+let currentDate = new Date().toJSON().slice(0, 10);
+
+const displayResponse = (response, res) => {
+  response.json().then((data) => {
+    const { articles } = data;
+    res.json({ articles: articles });
+  });
+};
+
+const allNews = (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  fetch(process.env.api_url_2)
+    .then((response) => {
+      displayResponse(response, res);
+    })
+    .catch((error) => {
+      res.send(
+        `<h2> oops! server is down please try again in few minutes </h2>`
+      );
+    });
+};
+
+const searchedNews = (req, res) => {
+  const { search } = req.params;
+  fetch(
+    `https://newsapi.org/v2/everything?q=+${search}&language=en&to=${currentDate}&pageSize=4&apiKey=${process.env.api_key}`
+  )
+    .then((response) => {
+      displayResponse(response, res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const categoryNews = (req, res) => {
+  const { category } = req.query;
+  console.log(req.query);
+  fetch(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=5&apiKey=${process.env.api_key}`
+  )
+    .then((response) => {
+      displayResponse(response, res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export { allNews, searchedNews, categoryNews };
--- server.js
+++ server.js
@@ -1,60 +1,20 @@
-import fetch from "node-fetch";
 import express from "express";
 import dotenv from "dotenv";
+import {
+  allNews,
+  searchedNews,
+  categoryNews,
+} from "./routesControllers/controllers.js";
 const app = express();
 const port = process.env.PORT || 7000;
 
 dotenv.config();
 
-let currentDate = new Date().toJSON().slice(0, 10);
+app.get("/", allNews);
 
-const displayResponse = (response, res) => {
-  response.json().then((data) => {
-    const { articles } = data;
-    res.json({ articles: articles });
-  });
-};
+app.get("/top-headlines/:search", searchedNews);
 
-app.get("/", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  fetch(process.env.api_url_2)
-    .then((response) => {
-      response.json().then((data) => {
-        const { articles } = data;
-        res.json({ articles: articles });
-      });
-    })
-    .catch((error) => {
-      res.send(
-        `<h2> oops! server is down please try again in few minutes </h2>`
-      );
-    });
-});
-
-app.get("/:search", (req, res) => {
-  const { search } = req.params;
-  fetch(
-    `https://newsapi.org/v2/everything?q=+${search}&language=en&to=${currentDate}&pageSize=4&apiKey=${process.env.api_key}`
-  )
-    .then((response) => {
-      displayResponse(response, res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-app.get("/category/:search", (req, res) => {
-  const { search } = req.params;
-  fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&category=${search}&apiKey=${process.env.api_key}`
-  )
-    .then((response) => {
-      displayResponse(response, res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+app.get("/top-headlines?", categoryNews);
 
 app.listen(port, () => {
   console.log(`server is listening at port:${port}  `);
